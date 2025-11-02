82f3fa477235bdfc9abfd4d47a5d67d526bbfe53
Laxmi Kanth Oruganti <oruganti.laxmikanth@gmail.com>
2025-11-02 01:51:01 -0500

    quick sort logic added

--- quick-sort.py
+++ quick-sort.py
@@ -3,8 +3,31 @@ import argparse
 # Quick Sort Implementation.
 def quick_sort(arr):
     print("Sorting array using Quick Sort...")
-    return arr
 
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) - 1]
+    less_than_pivot = []
+    greater_than_pivot = []
+    equal_to_pivot = []
+
+    print("pivot:", pivot)
+
+    for element in arr:
+        if element < pivot:
+            less_than_pivot.append(element)
+        elif element > pivot:
+            greater_than_pivot.append(element)
+        else:
+            equal_to_pivot.append(element)
+    # print("Less than pivot:", less_than_pivot)
+    # print("Greater than pivot:", greater_than_pivot)
+    # Recursively sort the less than and greater than pivot lists
+    sorted_less = quick_sort(less_than_pivot) if less_than_pivot else []
+    sorted_greater = quick_sort(greater_than_pivot) if greater_than_pivot else []
+
+    return sorted_less + equal_to_pivot + sorted_greater
 
 if __name__ == "__main__":
     print("Quick Sort Implementation")
